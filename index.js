3858b8e4e1a9d46c8e31316dc6e39134a9764d59
hxkim10 <abdelhakimtiabi@gmail.com>
2023-09-29 14:35:23 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let leftNumbers = DODGER.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    DODGER.style.left = `${left - 4}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = DODGER.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    DODGER.style.left = `${left + 4}px`;
+  }
+}
